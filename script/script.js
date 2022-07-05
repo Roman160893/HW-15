f582f75ecd811b39f4dfc41fabf24522965db95f
Roman Demchuk <4mmmcd@gmail.com>
2022-07-05 15:58:43 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,46 @@
+function* createIdGenerator() {
+   for (let i = 1; ; i++) {
+      yield i
+   };
+}
+
+const idGenerator = createIdGenerator();
+
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log('...');
+
+
+// Функція зміни шрифту
+
+
+function* newFontGenerator(el) {
+   for (let i = 1; ; i++) {
+      let fontSize = yield el;
+      if (fontSize === 'up') {
+         el += 2
+      } else if (fontSize === 'down') {
+         el -= 2
+      }
+   }
+}
+
+let ip = newFontGenerator(14);
+
+console.log(ip.next().value + 'px');
+console.log(ip.next('up').value + 'px');
+console.log(ip.next('up').value + 'px');
+console.log(ip.next().value + 'px');
+console.log(ip.next('down').value + 'px');
+console.log(ip.next('down').value + 'px');
+console.log(ip.next('down').value + 'px');
+console.log(ip.next().value + 'px');
